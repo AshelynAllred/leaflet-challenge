03d9a0cd3e7944762f158e141c07f1b022ee42a3
AshelynAllred <AshelynAllred@gmail.com>
2024-01-29 21:02:37 -0600

    Cleaned up code, filled out readme
    - Revised comment documentation
    - Added attributions for various layers where they did not exist previously
    - Filled out readme, copied over attributions and added in markdown
    - Added work citations, revised main tagline
    - Added image of map to readme

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,12 +2,14 @@
 const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson'
 
 // defining this function so we don't repeat ourselves
+// adds a waterfall if/else statement to determine bubble marker color.
 function getColor(d) {
   return  d > 90 ? 'red' :
           d > 70 ? 'darkorange' :
           d > 50 ? 'orange' :
           d > 30 ? 'yellow' :
-          d > 10 ? 'lime' : 'green'
+          d > 10 ? 'lime' : 
+          'green'
 }
 
 d3.json(url).then(function(data) {
@@ -17,15 +19,10 @@ d3.json(url).then(function(data) {
 
   //console.log(data.features[0].geometry.coordinates);
 
-  //defining a function to deal with each feature from the d3 
-  // function eachFeature(feature, layer) {
-  //   layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-  // }
-
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
+  // Create a GeoJSON layer that contains the features array on the eqData object.
   let eqData = L.geoJSON(features, {
-    // onEachFeature: eachFeature,
+    
+    //Styling for each bubble that gets plotted on the map
     style: function (feature) {
       let mag = feature.properties.mag;
       let depth = feature.geometry.coordinates[2];
@@ -35,38 +32,36 @@ d3.json(url).then(function(data) {
        color: "black",
        weight: 1,
        fillOpacity: .6,
+       // Decided on an exponential function for scaling since that's what earthquake magnitudes correspond to
        radius: mag ** 2,
        fillColor : getColor(depth)
       };
     },
     pointToLayer: function(geoJsonPoint, latlng) {
       return L.circleMarker(latlng);
-    }
-  }).bindPopup(function (layer) {  
+    },
+    //adding attribution for earthquake data for transparency reasons
+    attribution: 'Earthquake Data: <a href="https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php">USGS GeoJSON Summary</a> | M2.5+ Past 7 Days'
+  }).bindPopup(function (layer) {
     let place = layer.feature.properties.place;
     let mag = layer.feature.properties.mag;
     let depth = layer.feature.geometry.coordinates[2];
     let time = new Date(layer.feature.properties.time).toLocaleString();
+    //Popup message on click, included for informational purposes
     return `<h4>${place}<br>Magnitude: ${mag}<br>Depth: ${depth.toFixed(2)}km<br>${time}</h4>`;
   });
 
-  // tec
+  // tectonic plate boundary data
   let plateData = L.geoJSON(tectonicPlateData, {
     style: function (feature) {
       return {
-       color: '#e46939',
-       weight: 2,
+        color: '#e46939', // line color
+        weight: 2, // line thickness
       };
-    }
+    },
+    //adding attribution for tectonic plate data since it has an attribution license
+    attribution: 'Tectonic Plate Data: <a href="https://github.com/fraxen/tectonicplates">Hugo Ahlenius on GitHub</a> - Data Sources: Hugo Ahlenius; Nordpil; Peter Bird - (<a href="https://opendatacommons.org/licenses/by/1-0/">ODC-By</a>) v1.0'
   });
-
-  // for (let i = 0; i < features.length; i++) {
-  //   let location = features[i].geometry;
-  //   if (location) {
-  //     //console.log(location);
-  //     eqArray.push([location.coordinates[1], location.coordinates[0]]);
-  //   }
-  // }
   
   // Create the base layers.
   let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -91,7 +86,7 @@ d3.json(url).then(function(data) {
     
   // Create our map, giving it the streetmap and earthquakes layers to display on load.
   let myMap = L.map("map", {
-    center: [ 54, -100 ],
+    center: [ 20, -30 ],
     zoom: 3,
     layers: [street, plateData, eqData]
   });
@@ -103,22 +98,27 @@ d3.json(url).then(function(data) {
     collapsed: false
   }).addTo(myMap);
 
+  // Create a legend that displays the colors used for depth indication
   var legend = L.control({position: 'bottomright'});
     legend.onAdd = function (map) {
 
+    //create a div using the DomUtil and pass in the appropriate classes. One of these classes is used in custom.css for formatting
     var div = L.DomUtil.create('div', 'info legend');
     labels = ['<strong>Earthquake Depth</strong>'],
-    //categories were selected to go past breaking points in colors
-    categories = [91, 71, 51, 31, 11, 9];
+    //numerical categories were selected to go past break point in the getColor() logic
+    categories = [91, 71, 51, 31, 11, 10];
 
+    //for loop to go through all categories and assign labels dynamically (">y" for first category, "x-y"range between breakpoints in middle, "<x" for last category)
     for (var i = 0; i < categories.length; i++) {
             div.innerHTML += labels.push( '<i class="circle" style="background:' + getColor(categories[i]) + '"></i> ' 
             + (i === 0 ? ">" + (categories[i] - 1) : 
             i < categories.length - 1 ? (categories[i]-1) + "-" + (categories[i-1]-1) : 
-            "<" + (categories[i]+1)));
+            "<" + (categories[i])));
         }
         div.innerHTML = labels.join('<br>');
     return div;
     };
+
+    //add the legend to the map
     legend.addTo(myMap);
 });
